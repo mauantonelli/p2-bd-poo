52ec7190b11aa4b7cd7e21ad7a7de77826f64cc5
mauantonelli <pablojuanang@gmail.com>
2024-11-13 12:48:18 -0300

    itemvenda usecase

--- src/main/java/domain/repository/ItemVendaRepository.java
+++ src/main/java/domain/repository/ItemVendaRepository.java
@@ -2,4 +2,7 @@ package domain.repository;
 import domain.entity.ItemVenda;
 
 public interface ItemVendaRepository extends Repository<ItemVenda> {
+    void save(ItemVenda itemVenda);
+    void update(ItemVenda itemVenda);
+    void delete(ItemVenda itemVenda);
 }
--- src/main/java/domain/usecases/ItemVendaUseCase.java
+++ src/main/java/domain/usecases/ItemVendaUseCase.java
@@ -9,5 +9,17 @@ public class ItemVendaUseCase {
         this.repository = repository;
     }
 
+    public void save(ItemVenda itemVenda){
+        repository.save(itemVenda);
+    }
+
+    public void update(ItemVenda itemVenda){
+        repository.update(itemVenda);
+    }
+
+    public void delete(ItemVenda itemVenda){
+        repository.delete(itemVenda);
+    }
+
 
 }

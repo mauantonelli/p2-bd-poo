a495ae3ff79c06f5169913c54b9a7161aca24796
mauantonelli <pablojuanang@gmail.com>
2024-11-14 08:23:11 -0300

    sql dao

--- src/main/java/adapters/dao/ItemVendaDAOimpl.java
+++ src/main/java/adapters/dao/ItemVendaDAOimpl.java
@@ -1,16 +1,38 @@
 package adapters.dao;
+
 import adapters.ConnectionFactory;
 import domain.entity.ItemVenda;
+import domain.entity.Produto;
 import domain.entity.Venda;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class ItemVendaDAOimpl implements ItemVendaDAO {
 
     @Override
-    public void save(ItemVenda type) {
+    public void save(ItemVenda itemVenda) {
+        String sql = "INSERT INTO ItemVenda (idItemVenda, quantidadeVendida, valorTotal, produto_id, venda_id) VALUES (?, ?, ?, ?, ?)";
 
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, itemVenda.getIdItemVenda());
+            pstmt.setInt(2, itemVenda.getQuantidadeVendida());
+            pstmt.setDouble(3, itemVenda.getValorTotal());
+            pstmt.setInt(4, itemVenda.getProduto().getIdProduto());
+            pstmt.setInt(5, itemVenda.getVenda().getIdVenda());
+
+            pstmt.executeUpdate();
+            System.out.println("ItemVenda salvo com sucesso!");
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao salvar ItemVenda: " + e.getMessage());
+            e.printStackTrace(); // Para rastrear detalhes do erro
+        }
     }
 
     @Override
